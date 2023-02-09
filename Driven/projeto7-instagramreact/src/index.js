f17387d022365032ac6c9af366b3ca47fe816ad5
Cleiver <creibis10@gmail.com>
2023-02-08 22:19:08 -0300

    removendo variaveis inutilizadas

--- Driven/projeto7-instagramreact/src/index.js
+++ Driven/projeto7-instagramreact/src/index.js
@@ -2,22 +2,5 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-
-const storiesUsuarios = [
-    {imagem: "assets/img/9gag.svg", usuario: "9gag"}, 
-    {imagem: "assets/img/meowed.svg", usuario: "meowed"},
-    {imagem: "assets/img/barked.svg", usuario: "barked"},
-    {imagem: "assets/img/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet"},
-    {imagem: "assets/img/wawawicomics.svg", usuario: "wawawicomics"},
-    {imagem: "assets/img/respondeai.svg", usuario: "respondeai"},
-    {imagem: "assets/img/filomoderna.svg", usuario: "filomoderna"},
-    {imagem: "assets/img/memeriagourmet.svg", usuario: "memeriagourmet"}
-];
-
-const postsUsuarios = [
-    {imagem: "assets/img/dog.svg", usuario: "barked", perfil: "assets/img/barked.svg"}, 
-    {imagem: "assets/img/gato-telefone.svg", usuario: "meowed", perfil: "assets/img/meowed.svg"}
-];
-
 const bodyDom = document.querySelector("body");
 ReactDOM.render(App(), bodyDom);
