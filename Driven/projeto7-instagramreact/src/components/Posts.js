ac3df6c66f9929c67733df084f61c59e1aec69e2
Cleiver <creibis10@gmail.com>
2023-02-09 22:24:03 -0300

    requisitos obrigatorios e 1 bonus implementados

--- Driven/projeto7-instagramreact/src/components/Posts.js
+++ Driven/projeto7-instagramreact/src/components/Posts.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const postsUsuarios = [
     {imagem: "assets/img/dog.svg", usuario: "barked", perfil: "assets/img/barked.svg"}, 
     {imagem: "assets/img/gato-telefone.svg", usuario: "meowed", perfil: "assets/img/meowed.svg"}
@@ -17,43 +19,64 @@ export default function Posts(){
 }
 
 function Post (props){
+
+    const [salvo, setSalvo] = React.useState("bookmark-outline");
+    const [like, setLike] = React.useState("heart-outline");
+    const [likedColor, setLikedColor] = React.useState("");
+
+    function clickLike(){
+        if(like == "heart"){
+            setLike("heart-outline");
+            setLikedColor("");
+        }else{
+            setLike("heart");
+            setLikedColor("red")
+        }
+    }
+
+    function clickLikeImg(){
+        setLike("heart");
+        setLikedColor("red");
+    }
+
     return (
         <div class="post">
-                        <div class="topo">
-                            <div class="usuario">
-                                <img src={props.perfil} alt={props.usuario}/>
-                                {props.usuario}
-                            </div>
-                            <div class="acoes">
-                                <ion-icon name="ellipsis-horizontal"></ion-icon>
-                            </div>
-                        </div>
+            <div class="topo">
+                <div class="usuario">
+                    <img src={props.perfil} alt={props.usuario}/>
+                    {props.usuario}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
 
-                        <div class="conteudo">
-                            <img src={props.imagem} alt={props.usuario}/>
-                        </div>
+            <div class="conteudo">
+                <img onDoubleClick={clickLikeImg} src={props.imagem} alt={props.usuario}/>
+            </div>
 
-                        <div class="fundo">
-                        
-                            <div class="acoes">
-                                <div>
-                                    <ion-icon name="heart-outline"></ion-icon>
-                                    <ion-icon name="chatbubble-outline"></ion-icon>
-                                    <ion-icon name="paper-plane-outline"></ion-icon>
-                                </div>
-                                <div>
-                                    <ion-icon name="bookmark-outline"></ion-icon>
-                                </div>
-                            </div>
-
-                            <div class="curtidas">
-                                <img src="assets/img/respondeai.svg" alt="respondeai"/>
-                                <div class="texto">
-                                    Curtido por <strong>respondeai</strong> e <strong>outras <span>101.523</span> pessoas</strong>
-                                </div>
-                            </div>
-
-                        </div>
+            <div class="fundo">
+            
+                <div class="acoes">
+                    <div>
+                        <ion-icon class={likedColor} onClick={clickLike} name={like}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
+                    <div>
+                        {/* utilizando mudança de outline pra sinalizar item salvo */}
+                        <ion-icon onClick={() => setSalvo(salvo == "bookmark" ? "bookmark-outline" : "bookmark")} name={salvo}></ion-icon>
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src="assets/img/respondeai.svg" alt="respondeai"/>
+                    <div class="texto">
+                        Curtido por <strong>respondeai</strong> e <strong>outras <span>101.523</span> pessoas</strong>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
--- Driven/projeto7-instagramreact/src/components/Usuario.js
+++ Driven/projeto7-instagramreact/src/components/Usuario.js
@@ -1,11 +1,31 @@
+import React from 'react';
+
 export default function Usuario (props){
+    
+    const [nome, setNome] = React.useState(props.nome);
+    const [imagem, setImagem] = React.useState(props.imagem);
+    
+    function trocarNome(){
+        const novoNome = prompt("Digite o novo nome");
+        if(novoNome){
+            setNome(novoNome);
+        }
+    }
+
+    function trocarImagemUsuario(){
+        const novaImagem = prompt("Digite a nova imagem");
+        if(novaImagem){
+            setImagem(novaImagem);
+        }
+    }
+
     return (
         <div class="usuario">
-            <img src={props.imagem} alt="imagem de perfil"/>
+            <img onClick={trocarImagemUsuario} src={imagem} alt="imagem de perfil"/>
             <div class="texto">
                 <span>
-                    <strong>{props.nome}</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{nome}</strong>
+                    <ion-icon onClick= {trocarNome} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
